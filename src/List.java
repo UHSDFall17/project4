31c8fdbf09bc7850c0830d2609573c574e3f8c1c
Mike Schulenberg <mike.schulenberg@gmail.com>
2017-09-20 22:46:17 -0500

    Initial commit.

---
+++ src/List.java
@@ -0,0 +1,14 @@
+
+public class List 
+{
+	private String name;
+	private int task_num;
+	private Card card_list[];
+	
+	public List()
+	{
+		name = "N/A";
+		task_num = 0;
+		card_list = new Card[100];
+	}	
+}
