26e7c133475b5e63aaa30a32e7139db69f170fbd
Tien Nguyen <tiennguyen0530@hotmail.com>
2017-09-26 18:01:45 -0500

    tien commit

--- src/List.java
+++ src/List.java
@@ -1,14 +1,62 @@
+import java.util.ArrayList;
 
 public class List 
 {
 	private String name;
+	private int index; // this is used to initially store into the card_list array and to keep track on the array's size 
 	private int task_num;
-	private Card card_list[];
-	
+	private ArrayList<Card> cardList = new ArrayList<Card>(); 
+
 	public List()
 	{
 		name = "N/A";
+		index = 0;
 		task_num = 0;
-		card_list = new Card[100];
 	}	
+
+	public void addToList(Card cardInput)
+	{
+		cardList.add(cardInput);
+	}
+
+	public int getCardListSize()
+	{
+		return cardList.size();
+	}
+
+	public Card getCardListElement(int index)
+	{
+		return cardList.get(index);
+	}
+
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+
+	}
+
+	public int getTask_num() {
+		return task_num;
+	}
+
+	public void setTask_num(int task_num) {
+
+		this.task_num = task_num;
+
+	}
+
+
+
+	
+
+	
+
+	
+
+	
+
 }
