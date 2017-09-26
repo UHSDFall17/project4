c2efa058a7014addcd264c995bf7412aa5d3465f
Tien Nguyen <tiennguyen0530@hotmail.com>
2017-09-26 12:32:06 -0500

    Added arrayList to List class Added a View class that will display all the Cards Added a couple of methods to List and Card class

--- src/Project4.java
+++ src/Project4.java
@@ -2,7 +2,19 @@
 public class Project4 
 {
 	public static void main(String[] args)
-	{
-		System.out.println("Hello World");
+	{	
+		Card card1 = new Card( "tit", "dis","com");
+		Card card2 = new Card( "tit2", "dis2","com2");
+		List list1 = new List();
+		View view = new View();
+		
+		
+		list1.addToList(card1);
+		list1.addToList(card2);
+		
+		view.printCard(list1);
+		
+
+
 	}
 }
---
+++ src/View.java
@@ -0,0 +1,14 @@
+
+public class View {
+
+	public void printCard(List listInput){
+		
+		for( int i = 0; i < listInput.getCardListSize(); i++ ){
+			System.out.println(listInput.getCardListElement(i).getTitle());
+			System.out.println(listInput.getCardListElement(i).getDescription());
+			System.out.println(listInput.getCardListElement(i).getComment());		
+		}
+		
+	}
+	
+}
