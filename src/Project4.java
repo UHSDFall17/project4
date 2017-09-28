14c388436045a1d5601d33bea957c36187223d67
mcabrera108 <31631896+mcabrera108@users.noreply.github.com>
2017-09-28 07:09:36 -0500

    Update Project4.java

--- src/Project4.java
+++ src/Project4.java
@@ -1,8 +1,58 @@
+import java.util.Scanner;
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Project4 
 {
 	public static void main(String[] args)
 	{
+		String text_username,text_password,input1,input2;
+		int try_counter = 0;
+		boolean successful_login = false;
+		
+		while(try_counter < 3 && successful_login == false){
+			try_counter+=1;
+			if(try_counter == 3)
+			{
+				System.out.println("Too many unsuccessful events! Terminating program.");
+			}
+			
+			System.out.println("Type in your username and password to access your account.");
+			System.out.print("Username: ");
+			Scanner user_input = new Scanner(System.in); 
+			input1 = user_input.next();
+			
+			System.out.println();
+			System.out.print("Password: ");
+			Scanner pass_input = new Scanner(System.in);
+			input2 = user_input.next();
+			
+				Scanner sc2 = null;
+			    try {
+			        sc2 = new Scanner(new File("login.txt"));
+			        
+			    } catch (FileNotFoundException e) {
+			        e.printStackTrace();  
+			    }
+			    while (sc2.hasNextLine()) {
+			            Scanner s2 = new Scanner(sc2.nextLine());
+			        while (s2.hasNext()) {
+			            text_username = s2.next();
+			            text_password = s2.next();
+			            
+			            if(text_username.equals(input1) && text_password.equals(input2))
+			            {
+			            	System.out.println("Welcome " + text_username + "!");
+			            	successful_login = true;
+			            }
+			            
+			        }
+			    }
+		}
+		
 		Comment comment1 = new Comment("comment1comment1comment1comment1comment1comment1");
 		Comment comment2 = new Comment("comment2comment2comment2comment2comment2comment2comment2");
 		Comment comment3 = new Comment("comment3comment3comment3comment3");
